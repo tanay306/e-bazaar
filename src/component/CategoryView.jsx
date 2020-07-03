aaecff1fade0674b2cd2dbad260178e2aa3d6256
varunm14 <mehtavarunj@gmail.com>
2020-07-03 19:38:07 +0530

    category view updated

--- src/component/CategoryView.jsx
+++ src/component/CategoryView.jsx
@@ -1,37 +1,59 @@
-import React from 'react';
-import TreeMenu from 'react-simple-tree-menu';
-import Card from 'react-bootstrap/Card';
-import styles from './CategoryView.module.css';
+import React from "react";
+import TreeMenu from "react-simple-tree-menu";
+import Card from "react-bootstrap/Card";
+import styles from "./CategoryView.module.css";
 
-const CategoryView=()=>{
-    const treeData = [
+const CategoryView = () => {
+  const treeData = [
+    {
+      key: "Apparels",
+      label: "Apparels",
+
+      nodes: [
         {
-          key: 'first-level-node-1',
-          label: 'Node 1 at the first level',
-        
+          key: "Shirt",
+          label: "Shirt",
           nodes: [
             {
-              key: 'second-level-node-1',
-              label: 'Node 1 at the second level',
-              nodes: [
-                {
-                  key: 'third-level-node-1',
-                  label: 'Last node of the branch',
-                  nodes: [] // you can remove the nodes property or leave it as an empty array
-                },
-              ],
+              key: "Formal",
+              label: "Formal",
+            },
+            {
+              key: "Casual",
+              label: "Casual",
             },
           ],
         },
-        {
-          key: 'first-level-node-2',
-          label: 'Node 2 at the first level',
-        },
-      ];
-      return (<div className={styles.CategoryView}><Card style={{ width: '50%' }}>
-      <TreeMenu data={treeData} onClickItem={(e) => console.log(e)}/>
+      ],
+    },
+    {
+      key: "Electronics",
+      label: "Electronics",
 
-    </Card></div>
-      );
-}
+      nodes: [
+        {
+          key: "Mobile",
+          label: "Mobile",
+        },
+        {
+          key: "Television",
+          label: "Television",
+          // you can remove the nodes property or leave it as an empty array
+        },
+        {
+          key: "Refrigerator",
+          label: "Refrigerator",
+          // you can remove the nodes property or leave it as an empty array
+        },
+      ],
+    },
+  ];
+  return (
+    <div className={styles.CategoryView}>
+      <Card style={{ width: "80%" }}>
+        <TreeMenu data={treeData} onClickItem={(e) => console.log(e)} />
+      </Card>
+    </div>
+  );
+};
 export default CategoryView;
